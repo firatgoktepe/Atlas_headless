a64e4cd52b3b13abb59dfaac6ddab8da3af010ae
Firat Göktepe <firat@naxnordic.com>
2021-05-08 17:51:32 +0300

    changed front page

--- wp-templates/_loader.tsx
+++ wp-templates/_loader.tsx
@@ -3,7 +3,7 @@ import type { NextTemplate } from '@wpengine/headless/next';
 
 const templates: Templates<NextTemplate> = {
   '404': import('./404'),
-  'front-page': import('./front-page'),
+  'front-pagee': import('./front-pagee'),
   index: import('./index'),
   category: import('./category'),
   page: import('./page'),
