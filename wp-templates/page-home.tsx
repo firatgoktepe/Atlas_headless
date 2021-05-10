7bee957828d1e1b16898e3d7539de8ccb8219bb8
Firat Göktepe <firat@naxnordic.com>
2021-05-10 12:44:18 +0300

    added new home page template

---
+++ wp-templates/page-home.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useGeneralSettings } from '@wpengine/headless/react';
+import { usePost, getNextStaticProps } from '@wpengine/headless/next';
+import { Header, Hero, Footer } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import { getApolloClient } from '@wpengine/headless';
+import { GetStaticPropsContext } from 'next';
+
+const queryDevelopers = gql`
+    {
+      page(id: "cG9zdDoyNQ==") {
+        developers {
+          job
+          message
+          name
+        }
+      }
+    }
+  `;
+
+export default function Page(): JSX.Element {
+  const post = usePost();
+  const settings = useGeneralSettings();
+
+  const { data } = useQuery(queryDevelopers);
+  const developers = data?.page?.developers ?? [];
+ console.log({data});
+  console.log(developers);
+   
+  
+  return (
+    <>
+      <Header title={settings?.title} description={settings?.description} />
+      <main className="content content-page">
+        {post?.title && <Hero title={post?.title} />}
+        <div className="wrap">
+          {post && (
+            <div>
+              <div>
+                {/* eslint-disable-next-line react/no-danger */}
+                <div dangerouslySetInnerHTML={{ __html: post.content ?? '' }} />
+              </div>
+            </div>
+          )}
+        </div>
+
+        <h3>{developers.name}</h3>
+        <h4>{developers.job}</h4>
+        <p>{developers.message}</p>
+
+      </main>
+
+      <Footer copyrightHolder={settings?.title} />
+    </>
+  );
+};
+
+interface StaticPropsResult {
+  revalidate?: number | boolean;
+}
+
+export async function getStaticProps(context: GetStaticPropsContext){
+  const client = getApolloClient(context);
+  await client.query({
+    query: queryDevelopers,
+  });
+
+  const props = (await getNextStaticProps(context)) as StaticPropsResult;
+
+  props.revalidate = 1;
+
+  return props;
+}
