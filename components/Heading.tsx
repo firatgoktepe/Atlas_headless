2fe022f30f974a1ad4abea8a23adfee7075dc57d
Firat Göktepe <firat@naxnordic.com>
2021-05-11 19:02:06 +0300

    some changes for headings

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 // HeadingProps constrains headings to levels h1-h6.
 interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
